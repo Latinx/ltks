7f47ea4e51192da560fba3bdcf6e103077931c90
foe <ralphy817@gmail.com>
2026-08-28 08:42:36 -0400

    Bump to 12.1.8

--- ltks.lua
+++ ltks.lua
@@ -2598,7 +2598,7 @@ function events:CHAT_MSG_SYSTEM(msg, ...)
 end
 
 function ltks:OnEnable()
-	if ltks.db.profile.dopve then ltks:Print("[LTKS-LOAD] code version=" .. version .. " toc=12.1.7") end
+	if ltks.db.profile.dopve then ltks:Print("[LTKS-LOAD] code version=" .. version .. " toc=12.1.8") end
 	--self:RegisterEvent("CHAT_MSG_ADDON", "AddonMessageHandler")
 	--OnEvent runs the function events:event
 	frame:SetScript("OnEvent", function(self, event, ...)
